ef6e5a74d543c3260f08113a1f0f52657522cbf5
Nikhil <nikhillokare6@gmail.com>
2023-05-18 21:03:08 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template,request
+import pandas as pd
+import numpy as np
+import pickle
+app = Flask(__name__)
+data = pd.read_csv('cleaned_data.csv')
+pipe = pickle.load(open("RidgeModel.pkl",'rb'))
+@app.route('/')
+def index():
+
+    locations = sorted(data['location'].unique())
+    return render_template('index.html',locations=locations)
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    location = request.form.get('location')
+    bhk = request.form.get('bhk')
+    bath = request.form.get('bath')
+    sqft = request.form.get('total_sqft')
+    print(location,bhk,bath,sqft)
+
+    input = pd.DataFrame([[location,bhk,bath,sqft]],columns=['location','bhk','bath','total_sqft'])
+    prediction = pipe.predict(input)[0] * 100000;
+
+    return str(np.round(prediction,2))
+
+if __name__ == '__main__':
+    app.run(debug=True,port=4000)
